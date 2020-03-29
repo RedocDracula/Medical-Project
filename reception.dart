20599a55ca6693310a42a8cf5177b5a1646260b0
Vighaneshs <39428595+Vighaneshs@users.noreply.github.com>
2020-03-29 18:21:20 +0530

    Update reception.dart

--- reception.dart
+++ reception.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
+class Doctor {
+  final String name;
+
+  Doctor(this.name);
+
+  static List<Doctor> getDoctors() {
+    List<Doctor> items = <Doctor>[];
+    items.add(
+        Doctor(
+          "Doctor 1",
+        )
+    );
+    items.add(
+        Doctor(
+          "Doctor 2",
+        )
+    );
+    return items;
+  }
+
+
+}
+
 class Patient {
   final String name;
   final String description;
@@ -65,38 +88,89 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-class PatientPage extends StatelessWidget {
-  PatientPage({Key key, this.item}) : super(key: key);
+
+class PatientPage extends StatefulWidget {
   final Patient item;
+  PatientPage({Key key, this.item}) : super(key: key);
+  @override
+  _PatientPage createState() => _PatientPage(item: this.item);
+
+}
+
+class _PatientPage extends State<PatientPage> {
+  final Patient item;
+  _PatientPage({Key key, this.item}) ;
+  final myController = TextEditingController();
+  int _selectedDoctor = 0;
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Fill this out in the next step.
     return Scaffold(
       appBar: AppBar(
         title: Text(this.item.name),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Container(
-          padding: EdgeInsets.all(0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            padding: EdgeInsets.all(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.description),
-                          ],
-                        )
-                    )
-                )
-              ]
-          ),
-        ),
+                Text("Name of patient: "),
+                Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("Symptoms: "),
+                Text(this.item.description, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("Forward to: "),
+                new DropdownButton(
+                  hint: new Text('Select Doctor'),
+                  items: <String>['Doctor1', 'Doctor2', 'Doctor3', 'Doctor4']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedDoctor = value;
+                    });
+                  },
+                  isExpanded: true,
+                ),
+                Text("Comments: "),
+                TextField(
+                  controller: myController,
+                ),
+
+              ],
+            )
+        )
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        // When the user presses the button, show an alert dialog containing
+        // the text that the user has entered into the text field.
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                // Retrieve the text the that user has entered by using the
+                // TextEditingController.
+                content: Text("Sent to Doctor2\n" +"Comment : " + myController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: Icon(Icons.send),
       ),
     );
   }
@@ -120,7 +194,9 @@ class PatientBox extends StatelessWidget {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)), Text(this.item.description),
+                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(this.item.description),
+
                           ],
                         )
                     )
@@ -131,3 +207,5 @@ class PatientBox extends StatelessWidget {
     );
   }
 }
+
+
